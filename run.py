84fde352978bc52793070044b78be09acdf0f6d1
SofianChay <sofian.chaybouti@gmail.com>
2020-07-24 07:02:47 +0000

    add rocstar

--- run.py
+++ run.py
@@ -26,24 +26,28 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 map_losses = {"bce": nn.CrossEntropyLoss, "focal": FocalLoss, 'rocstar': RocStar, "margin": MarginFocalLoss}
 
 
-def training_step(epoch, optimizer, train_dataloader, model, criterion, scheduler, device, loss, last_epoch_y_pred=None, last_epoch_y_t=None):
+def training_step(epoch, optimizer, train_dataloader, model, criterion, scheduler, device, name_loss, last_epoch_y_pred=None, last_epoch_y_t=None):
     loss = 0
     model.train()
     tk = tqdm(train_dataloader, total=len(train_dataloader), unit="batch")
     total_examples = 0
-    # if epoch == 0 and loss == 'rocstar':
-    #     last_epoch_y_pred = torch.tensor( 1.0 - numpy.random.rand(len(train_dataloader))/2.0, dtype=torch.float, device=device)
-    #     last_epoch_y_t    = torch.tensor([o for o in train_tt],dtype=torch.float, device=device)
-    # if loss == 'rocstar':
-    #     epoch_y_pred=[]
-    #     epoch_y_t=[]
+
+    if epoch == 0 and name_loss == 'rocstar':
+        last_epoch_y_pred = torch.tensor( 1.0 - np.random.rand(len(train_dataloader))/2.0, dtype=torch.float, device=device)
+        last_epoch_y_t    = torch.tensor(np.random.randint(0, 2, len(train_dataloader)),dtype=torch.float, device=device)
+    if name_loss == 'rocstar':
+        epoch_y_pred = []
+        epoch_y_t = []
+    else:
+        epoch_y_pred, epoch_y_t = None, None
+
     for i, batch in enumerate(tk):
         total_examples += len(batch[0])
         optimizer.zero_grad()
         logits = model(input_ids=batch[0].to(device), attention_mask=batch[1].to(device), visual_embeddings=batch[3].to(device))
-        if loss == 'rocstar':
-            preds = F.softmax(logits, dim=1)
-            batch_loss = criterion(_y_true=batch[2].to(device), y_pred=preds, _epoch_true=last_epoch_y_t, epoch_pred=last_epoch_y_pred)
+        if name_loss == 'rocstar':
+            preds = torch.sigmoid(logits)
+            batch_loss = criterion(_y_true=batch[2].to(device), y_pred=preds, _epoch_true=last_epoch_y_t.to(device), epoch_pred=last_epoch_y_pred.to(device))
         else:
             batch_loss = criterion(logits, batch[2].to(device))
         loss += batch_loss.item() 
@@ -52,12 +56,14 @@ def training_step(epoch, optimizer, train_dataloader, model, criterion, schedule
         optimizer.step()
         scheduler.step()
         tk.set_postfix({"average loss": loss / total_examples})
-        # if loss == 'rocstar':
-        #     epoch_y_pred.extend(pred)
-        #     epoch_y_t.extend(batch[2].to(device))
-    if loss == 'rocstar':
-        criterion.epoch_update_gamma()
-    return loss 
+        if name_loss == 'rocstar':
+            epoch_y_pred.extend(preds.cpu().tolist())
+            epoch_y_t.extend(batch[2].to(device).tolist())
+    if name_loss == 'rocstar':
+        epoch_y_t = torch.tensor(epoch_y_t)
+        epoch_y_pred = torch.tensor(epoch_y_pred)
+        criterion.epoch_update_gamma(epoch_y_t, epoch_y_pred)
+    return loss, epoch_y_pred, epoch_y_t
 
 
 def test(model, dev_dataloader):
@@ -73,10 +79,12 @@ def test(model, dev_dataloader):
         for i, batch in enumerate(tqdm(dev_dataloader, unit="batch")): 
             num_examples += len(batch[0])
             logits = model(input_ids=batch[0].to(device), attention_mask=batch[1].to(device), visual_embeddings=batch[3].to(device))
-            probs = F.sigmoid(logits)
-            # predicted_classes = probs.max(dim=1)
+            probs = torch.sigmoid(logits)
+            # probs = F.softmax(logits, dim=1)
+            # _, predicted_classes = probs.max(dim=1)
             predicted_classes = (probs >= .5)
             accuracy += torch.sum(predicted_classes.cpu() == batch[2]).item()
+            # confidences += probs[:, 1].cpu().tolist()
             confidences += probs.cpu().tolist()
             labels += batch[2].tolist()
     
@@ -84,7 +92,7 @@ def test(model, dev_dataloader):
     return roc_auc, accuracy / num_examples
 
 
-def train(train_dataloader, dev_dataloader, model, lr, num_epochs, loss):
+def train(train_dataloader, dev_dataloader, model, lr, num_epochs, name_loss):
     device = torch.device(f"cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     best_model = copy.deepcopy(model.state_dict())
@@ -95,12 +103,14 @@ def train(train_dataloader, dev_dataloader, model, lr, num_epochs, loss):
     optimizer = optim.AdamW(model.parameters(), lr=lr, eps=1e-8)
     total_steps = num_epochs * len(train_dataloader) 
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
-    criterion = map_losses[loss]()
+    criterion = map_losses[name_loss]()
     print(test(model, dev_dataloader))
 
     for epoch in range(num_epochs):
+        if epoch == 0:
+          epoch_y_pred, epoch_y_t = None, None 
         print(f"fine tuning step : {epoch + 1}")
-        loss = training_step(epoch, optimizer, train_dataloader, model, criterion, scheduler, device, loss)
+        loss, epoch_y_pred, epoch_y_t = training_step(epoch, optimizer, train_dataloader, model, criterion, scheduler, device, name_loss, epoch_y_pred, epoch_y_t)
         print(f"training loss : {loss}")
         auc_roc, accuracy = test(model, dev_dataloader)
         print(f'accuracy : {accuracy}, auc roc : {auc_roc}')
@@ -121,7 +131,7 @@ if __name__ == '__main__':
     parser.add_argument('--dev_batch_size', default=32, type=int)
     parser.add_argument('--num_models', default=12, type=int)
     parser.add_argument('--images_features', default='bottom_up')
-    parser.add_argument('--loss', default='margin', help='"margin", "focal", "bce", "rocstar"')
+    parser.add_argument('--loss', default='rocstar', help='"margin", "focal", "bce", "rocstar"')
 
     args = parser.parse_args()
 
--- utils/losses.py
+++ utils/losses.py
@@ -115,7 +115,7 @@ class RocStar(nn.Module):
 
         return res2
 
-    def epoch_update_gamma(self, y_true,y_pred, epoch=-1,delta=2):
+    def epoch_update_gamma(self, y_true, y_pred, delta=2):
         """
         Calculate gamma from last epoch's targets and predictions.
         Gamma is updated at the end of each epoch.
@@ -124,30 +124,31 @@ class RocStar(nn.Module):
         """
         DELTA = delta
         SUB_SAMPLE_SIZE = 2000.0
-        pos = y_pred[y_true==1]
-        neg = y_pred[y_true==0] # yo pytorch, no boolean tensors or operators?  Wassap?
+        pos = y_pred[y_true.nonzero()]
+        neg = y_pred[(1 - y_true).nonzero()]
+          # yo pytorch, no boolean tensors or operators ? Wassap ?
         # subsample the training set for performance
         cap_pos = pos.shape[0]
         cap_neg = neg.shape[0]
-        pos = pos[torch.rand_like(pos) < SUB_SAMPLE_SIZE/cap_pos]
-        neg = neg[torch.rand_like(neg) < SUB_SAMPLE_SIZE/cap_neg]
+        pos = pos[torch.rand_like(pos) < SUB_SAMPLE_SIZE / cap_pos]
+        neg = neg[torch.rand_like(neg) < SUB_SAMPLE_SIZE / cap_neg]
         ln_pos = pos.shape[0]
         ln_neg = neg.shape[0]
-        pos_expand = pos.view(-1,1).expand(-1,ln_neg).reshape(-1)
+        pos_expand = pos.view(-1, 1).expand(-1, ln_neg).reshape(-1)
         neg_expand = neg.repeat(ln_pos)
         diff = neg_expand - pos_expand
         ln_All = diff.shape[0]
-        Lp = diff[diff>0] # because we're taking positive diffs, we got pos and neg flipped.
+        Lp = diff[diff > 0] # because we're taking positive diffs, we got pos and neg flipped.
         ln_Lp = Lp.shape[0]-1
-        diff_neg = -1.0 * diff[diff<0]
+        diff_neg = -1.0 * diff[diff < 0]
         diff_neg = diff_neg.sort()[0]
-        ln_neg = diff_neg.shape[0]-1
+        ln_neg = diff_neg.shape[0] - 1
         ln_neg = max([ln_neg, 0])
-        left_wing = int(ln_Lp*DELTA)
-        left_wing = max([0,left_wing])
-        left_wing = min([ln_neg,left_wing])
+        left_wing = int(ln_Lp * DELTA)
+        left_wing = max([0, left_wing])
+        left_wing = min([ln_neg, left_wing])
         default_gamma=torch.tensor(0.2, dtype=torch.float, device=y_true.device)
-        if diff_neg.shape[0] > 0 :
+        if diff_neg.shape[0] > 0:
           self.gamma = diff_neg[left_wing]
         else:
           self.gamma = default_gamma # default=torch.tensor(0.2, dtype=torch.float).cuda() #zoink
