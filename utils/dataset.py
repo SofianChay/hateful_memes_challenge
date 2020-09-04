42136df5be2dcd1fc1ed7d9a160c2e8b6234684a
SofianChay <sofian.chaybouti@gmail.com>
2020-09-04 23:27:24 +0000

    add nlp model and averaging between nlp and multimodal

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,44 +2,17 @@ import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from torch.utils.data import Dataset
 import pickle
-from transformers import BertTokenizer, RobertaTokenizer
-from transformers import BertConfig, RobertaConfig
-
-# from fairseq.data.encoders.fastbpe import fastBPE
-# from fairseq.data import Dictionary
 
 import os
 import numpy as np
 from tqdm import tqdm 
 
-# Load model
-# config = RobertaConfig.from_pretrained(
-#     "/content/drive/My Drive/hateful_memes/bertweet/BERTweet_base_transformers/config.json"
-# )
-
-# # Load BPE encoder
-# # jsuis un pirate 
-# class Args:
-#   def __init__(self):
-#     self.bpe_codes = "/content/drive/My Drive/hateful_memes/bertweet/BERTweet_base_transformers/bpe.codes"
-
-
-
-# bpe = fastBPE(Args())
-
-# Load the dictionary  
-# vocab = Dictionary()
-# vocab.add_from_file("/content/drive/My Drive/hateful_memes/bertweet/BERTweet_base_transformers/dict.txt")
-
-
-config = BertConfig.from_pretrained('bert-base-uncased')
-tkz = BertTokenizer.from_pretrained('bert-base-uncased')
 
 class MyDataset(Dataset):
 
     def __init__(self, input_ids, attention_masks, images_features, labels, ids):
-        self.input_ids = torch.tensor(input_ids)
-        self.attention_masks = torch.tensor(attention_masks)
+        self.input_ids = torch.tensor(input_ids) # 1, 
+        self.attention_masks = torch.tensor(attention_masks)  # 1, 1, 0, 0, 1, 1
         self.images_features = images_features
         self.labels = labels
         self.ids = ids
@@ -48,11 +21,14 @@ class MyDataset(Dataset):
         return len(self.input_ids)
 
     def __getitem__(self, index):
-        return self.input_ids[index], self.attention_masks[index], self.labels[index], self.images_features[index], self.ids[index]
+        if self.images_features is not None:
+            return self.input_ids[index], self.attention_masks[index], self.labels[index], self.images_features[index], self.ids[index]
+        else:
+            return self.input_ids[index], self.attention_masks[index], self.labels[index], self.ids[index]
 
 
 
-def create(data, datatype, batch_size, images_features_dict):
+def create(data, datatype, batch_size, images_features_dict, tkz, config):
 
     input_ids = []
     attention_masks = []
@@ -62,8 +38,7 @@ def create(data, datatype, batch_size, images_features_dict):
 
     for elt in data:
         inputs = tkz.encode(elt['text'])
-        # subwords = '<s> ' + bpe.encode(elt['text']) + ' </s>'
-        # inputs = vocab.encode_line(subwords, append_eos=False, add_if_not_exist=False).long().tolist()
+
         max_len = max(max_len, len(inputs))
         input_ids.append(inputs)
         attention_masks.append([1] * len(inputs))
@@ -74,20 +49,28 @@ def create(data, datatype, batch_size, images_features_dict):
             labels.append(elt['label'])
     print(f'maximum meme text length : {max_len}')
 
-    images_features_list = []
-    for elt in data:
-      images_features_list.append(images_features_dict[elt['id']])
-      
-    max_len_features = max([len(features) for features in images_features_list])
-    attention_masks_images = []
+    if images_features_dict is not None:
+        images_features_list = []
+        for elt in data:
+            if elt['id'] in images_features_dict:
+                images_features_list.append(images_features_dict[elt['id']])
+          
+        max_len_features = max([len(features) for features in images_features_list])
+        attention_masks_images = []
 
-    for i in range(len(images_features_list)):
-        attention_masks_images.append([1] * len(images_features_list[i]) + [0] * (max_len_features  - len(images_features_list[i])))
-        images_features_list[i] = torch.cat((images_features_list[i], torch.zeros((max_len_features - len(images_features_list[i]), 2048))), dim=0)
+        for i in range(len(images_features_list)):
+            attention_masks_images.append([1] * len(images_features_list[i]) + [0] * (max_len_features  - len(images_features_list[i])))
+            images_features_list[i] = torch.cat((images_features_list[i], torch.zeros((max_len_features - len(images_features_list[i]), 2048))), dim=0)
 
-    for i in range(len(images_features_list)):
-        input_ids[i] += [config.pad_token_id] * (max_len - len(input_ids[i]))
-        attention_masks[i] += [0] * (max_len - len(attention_masks[i])) + attention_masks_images[i]
+        for i in range(len(images_features_list)):
+            input_ids[i] += [config.pad_token_id] * (max_len - len(input_ids[i]))
+            attention_masks[i] += [0] * (max_len - len(attention_masks[i])) + attention_masks_images[i]
+
+    else:
+        images_features_list = None
+        for i in range(len(attention_masks)):
+            input_ids[i] += [config.pad_token_id] * (max_len - len(input_ids[i]))
+            attention_masks[i] += [0] * (max_len - len(attention_masks[i]))
 
     dataset = MyDataset(input_ids, attention_masks, images_features_list, labels, ids)
 
--- utils/model.py
+++ utils/model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 from mmf.common.registry import registry
-
+from transformers import RobertaModel
 
 model_cls = registry.get_model_class("visual_bert")
 
@@ -11,20 +11,38 @@ class MyVisualBert(nn.Module):
 
     def __init__(self):
         super(MyVisualBert, self).__init__()
-        self.visual_bert = model_cls.from_pretrained("visual_bert.finetuned.hateful_memes.from_coco").model
-        # self.visual_bert = model_cls.from_pretrained("visual_bert.pretrained.coco").model
-        self.image_features = nn.Sequential(nn.Linear(2048, 2048), nn.ReLU(), nn.LayerNorm(2048))
+        self.visual_bert = model_cls.from_pretrained("visual_bert.pretrained.coco").model
+        # self.image_features = nn.Sequential(nn.Linear(2048, 2048), nn.ReLU(), nn.LayerNorm(2048))
         self.classifier = nn.Sequential(nn.Linear(768, 768), nn.ReLU(), nn.Dropout(.1), nn.LayerNorm(768), nn.Linear(768, 1))
-
     
     def forward(self, input_ids, attention_mask, visual_embeddings):
 
         device = input_ids.device
-        visual_embeddings = self.image_features(visual_embeddings)
-        _, pooled, _ = self.visual_bert.bert(input_ids=input_ids, # tokens
+        # visual_embeddings = self.image_features(visual_embeddings)
+        embs, pooled, _ = self.visual_bert.bert(input_ids=input_ids, # tokens
                                 attention_mask=attention_mask, # attention mask phrase + mot 
                                 visual_embeddings=visual_embeddings, # 2048
                                 visual_embeddings_type=torch.ones((input_ids.shape[0], visual_embeddings.shape[1]), dtype=torch.long).to(device))
-                                
+        
+        # embs = torch.sum(embs * attention_mask.unsqueeze(-1), dim=1) /torch.sum(attention_mask.unsqueeze(-1), dim=1)
+        logits = self.classifier(pooled)
+
+        return logits.squeeze(-1)
+
+
+class RobertaClassif(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.transformer = RobertaModel.from_pretrained('roberta-base')
+        self.classifier =  nn.Sequential(nn.Linear(768, 768), nn.ReLU(), nn.Dropout(.1), nn.LayerNorm(768), nn.Linear(768, 1))
+    
+    def forward(self, input_ids, attention_mask, visual_embeddings):
+
+        embs, pooled = self.transformer(input_ids, attention_mask)
         logits = self.classifier(pooled)
         return logits.squeeze(-1)
+
+
+
+
